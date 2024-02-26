ed34bba8bd99f6b1bdb1cfda4dbbf0535168dd88
Caris Williams <cwill1013@gmail.com>
2024-02-26 11:07:32 -0500

    Initial Commit

--- SelectionStatementExercise/Program.cs
+++ SelectionStatementExercise/Program.cs
@@ -4,7 +4,24 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int favNumber = 36;
+
+            //string input = Console.ReadLine();
+
+            int userGuess = int.Parse(Console.ReadLine());
+
+            if (userGuess == favNumber)
+            {
+                Console.WriteLine($"Lucky Guess!!!");
+            }
+            else if (userGuess > favNumber)
+            {
+                Console.WriteLine($"Too High!!!");
+            }
+            else
+            {
+                Console.WriteLine($"Too Low!!!");
+            }
         }
     }
 }
